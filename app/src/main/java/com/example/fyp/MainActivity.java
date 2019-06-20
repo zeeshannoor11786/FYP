6aadfa948226a23e82adc543d4b96eb37b95c7c9
Zeeshan Noor <bshani15@gmail.com>
2019-06-20 16:19:54 +0500

    three objects commit

--- app/src/main/java/com/example/fyp/MainActivity.java
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fyp;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -20,9 +21,9 @@ import com.google.ar.sceneform.ux.TransformableNode;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArFragment arFragment;
-    private ModelRenderable chairRenderable;
+    private ModelRenderable chairRenderable,bearRenderable,lionRenderable;
 
-    ImageView chair;
+    ImageView bear,lion,chair;
 
     View arrayView[];
     ViewRenderable name_animal;
@@ -39,7 +40,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //view
 
+         bear =(ImageView)findViewById(R.id.bear);
+         lion =(ImageView)findViewById(R.id.lion);
         chair =(ImageView)findViewById(R.id.chair);
+
+
+//         chair = (ImageView)findViewById(R.id.chair);
+
 
         setArrayView();
         setClickListner();
@@ -64,28 +71,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setupModel() {
-
-        ModelRenderable.builder()
-                .setSource(this,R.raw.chair)
-                .build().thenAccept(renderable -> chairRenderable = renderable)
-                .exceptionally(
-                        throwable -> {
-                            Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
-                            return null;
-                        }
-                );
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            ModelRenderable.builder()
+                    .setSource(this,R.raw.lion)
+                    .build().thenAccept(renderable -> lionRenderable = renderable)
+                   .exceptionally(
+                            throwable -> {
+                                Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
+                                return null;
+                            }
+                    );
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            ModelRenderable.builder()
+                    .setSource(this, R.raw.bear)
+                    .build().thenAccept(renderable -> bearRenderable = renderable)
+                    .exceptionally(
+                            throwable -> {
+                                Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
+                                return null;
+                            }
+                    );
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            ModelRenderable.builder()
+                    .setSource(this, R.raw.chair)
+                    .build().thenAccept(renderable -> chairRenderable = renderable)
+                    .exceptionally(
+                            throwable -> {
+                                Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
+                                return null;
+                            }
+                    );
+        }
     }
 
     private void createModel(AnchorNode anchorNode, int selected) {
 
         if(selected==1)
         {
+            TransformableNode lion = new TransformableNode(arFragment.getTransformationSystem());
+            lion.setParent(anchorNode);
+            lion.setRenderable(lionRenderable);
+            lion.select();
+        }
+        if(selected==2)
+        {
+            TransformableNode bear = new TransformableNode(arFragment.getTransformationSystem());
+            bear.setParent(anchorNode);
+            bear.setRenderable(bearRenderable);
+            bear.select();
+        }
+        if(selected==3)
+        {
             TransformableNode chair = new TransformableNode(arFragment.getTransformationSystem());
             chair.setParent(anchorNode);
-            chair.setRenderable(chairRenderable);
+            chair.setRenderable(bearRenderable);
             chair.select();
         }
+
     }
 
     private void setClickListner() {
@@ -97,12 +141,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void setArrayView(){
 
         arrayView = new View[]{
-          chair
+          bear,lion,chair
         };
     }
 
     @Override
     public void onClick(View v) {
 
+        if(v.getId()==R.id.lion ) {
+            selected = 1;
+        }
+        if(v.getId()==R.id.bear){
+            selected = 2;
+        }
+        if(v.getId()==R.id.chair){
+            selected = 3;
+        }
+
+        }
+
     }
-}
+
