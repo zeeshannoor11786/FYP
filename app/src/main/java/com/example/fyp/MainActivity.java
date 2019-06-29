761ad3401c3006e634955afaa0ccaf7df0357a17
Zeeshan Noor <bshani15@gmail.com>
2019-06-29 18:34:13 +0500

    mid year presentation here we go..

--- app/src/main/java/com/example/fyp/MainActivity.java
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -21,9 +21,9 @@ import com.google.ar.sceneform.ux.TransformableNode;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArFragment arFragment;
-    private ModelRenderable moonchairRenderable,bearRenderable,lionRenderable,blackbedRenderable;
+    private ModelRenderable moonchairRenderable,sofaRenderable,lionRenderable,blackbedRenderable;
 
-    ImageView bear,lion,moonchair,blackbed;
+    ImageView sofa,lion,moonchair,blackbed;
 
     View arrayView[];
     ViewRenderable name_animal;
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //view
 
-         bear =(ImageView)findViewById(R.id.bear);
-         lion =(ImageView)findViewById(R.id.lion);
+        sofa =(ImageView)findViewById(R.id.sofa);
+         lion =(ImageView)findViewById(R.id.cupboard);
         moonchair =(ImageView)findViewById(R.id.moonchair);
        blackbed =(ImageView)findViewById(R.id.blackbed);
 
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void setupModel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             ModelRenderable.builder()
-                    .setSource(this,R.raw.lion)
+                    .setSource(this,R.raw.cupboard)
                     .build().thenAccept(renderable -> lionRenderable = renderable)
                    .exceptionally(
                             throwable -> {
@@ -85,8 +85,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             ModelRenderable.builder()
-                    .setSource(this, R.raw.bear)
-                    .build().thenAccept(renderable -> bearRenderable = renderable)
+                    .setSource(this, R.raw.sofa)
+                    .build().thenAccept(renderable -> sofaRenderable = renderable)
                     .exceptionally(
                             throwable -> {
                                 Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
@@ -129,10 +129,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if(selected==2)
         {
-            TransformableNode bear = new TransformableNode(arFragment.getTransformationSystem());
-            bear.setParent(anchorNode);
-            bear.setRenderable(bearRenderable);
-            bear.select();
+            TransformableNode sofa = new TransformableNode(arFragment.getTransformationSystem());
+            sofa.setParent(anchorNode);
+           sofa.setRenderable(sofaRenderable);
+          sofa.select();
         }
         if(selected==3)
         {
@@ -142,8 +142,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             moonchair.select();
         }
         if(selected==4)
-        {
-            TransformableNode blackbed = new TransformableNode(arFragment.getTransformationSystem());
+        {            TransformableNode blackbed = new TransformableNode(arFragment.getTransformationSystem());
             blackbed.setParent(anchorNode);
             blackbed.setRenderable(blackbedRenderable);
             blackbed.select();
@@ -160,17 +159,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void setArrayView(){
 
         arrayView = new View[]{
-          bear,lion,moonchair,blackbed
+          sofa,lion,moonchair,blackbed
         };
     }
 
     @Override
     public void onClick(View v) {
 
-        if(v.getId()==R.id.lion ) {
+        if(v.getId()==R.id.cupboard ) {
             selected = 1;
         }
-        if(v.getId()==R.id.bear){
+        if(v.getId()==R.id.sofa){
             selected = 2;
         }
         if(v.getId()==R.id.moonchair){
